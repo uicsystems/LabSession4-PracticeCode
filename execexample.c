18b73a4df12d6d60f5087d3e9f8678a76d3ba973
KirtapGeno <pgundr2@uic.edu>
2018-09-28 12:58:48 -0500

    Update execexample.c

--- execexample.c
+++ execexample.c
@@ -5,11 +5,13 @@
 int main() {
 
   char *argsarray[] = {"/bin/ls", "-l", (char *)0};
+  printf("look \n");
   
   int pid = fork();
   if (pid == 0) {
     printf("Child with pid %d, about to exec ls\n", getpid());
     execv(argsarray[0], argsarray);
+    printf("look \n");
   } else {
     printf("I am the parent.  I am waiting for my child %d to die.\n", pid);
     int status;
